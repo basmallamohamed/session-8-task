a05769da65eea7aa809be4a5f4c6e08be88acb3c
basmallamohamed <basmalla.nassar@gmail.com>
2022-08-03 12:25:49 +0200

    first and completed commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+     $("#awe").mouseenter(function(){
+        document.getElementById("awe").style.color="blue";
+     });
+     $("#awe").mouseleave(function(){
+        document.getElementById("awe").style.color="black";
+     });
+});
